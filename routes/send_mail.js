a7f950d194b307e20394ed14ffdb16c61aa473ac
nodeexcel <vaibhav.excel2011@gmail.com>
2018-01-16 18:41:48 +0530

    test

--- routes/send_mail.js
+++ routes/send_mail.js
@@ -2,37 +2,43 @@ var nodemailer = require("nodemailer");
 var smtpTransport = require("nodemailer-smtp-transport");
 var express = require('express')
 var router = express.Router()
+var multer  = require('multer')
+var upload = multer()
+var multiparty = require('multiparty');
 
 router.post('/',function (req, res) {
-req.on('data', (data)=>{
-    console.log(data.toJSON());
-})
+    // console.log("payload",req)
+    var form = new multiparty.Form();
 
-    // let mailer = nodemailer.createTransport(smtpTransport({
-    //     host: req.body.smtp_server,
-    //     port: parseInt(req.body.server_port),
-    //     auth: {
-    //         user: req.body.username,
-    //         pass: req.body.password
-    //     }
-    // }));
-    // mailer.sendMail({
-    //     from: req.body.from,
-    //     to: req.body.to,
-    //     subject: req.body.subject,
-    //     html: req.body.html,
-    //     attachments: [{
-    //                 filename: req.body.filename,
-    //                 content: req.body.content
-    //             }]
-    // }, (error, response) => {
-    //     if (error) {
-    //         res.json({message:"message not sent successfully",status:0,error:error});
-    //     } else {
-    //         res.json({ message: "messsage sent successfully", status: 1, email_response: response, subject: req.body.subject, body: req.body.html });
-    //     }
-    //     mailer.close();
-    // });
+    form.parse(req, function(err, fields, files) {
+        console.log(fields, files,"===================")
+    });
+
+    let mailer = nodemailer.createTransport(smtpTransport({
+        host: req.body.smtp_server,
+        port: parseInt(req.body.server_port),
+        auth: {
+            user: req.body.username,
+            pass: req.body.password
+        }
+    }));
+    mailer.sendMail({
+        from: req.body.from,
+        to: req.body.to,
+        subject: req.body.subject,
+        html: req.body.html,
+        attachments: [{
+                    filename: req.body.filename,
+                    content: req.body.content
+                }]
+    }, (error, response) => {
+        if (error) {
+            res.json({message:"message not sent successfully",status:0,error:error});
+        } else {
+            res.json({ message: "messsage sent successfully", status: 1, email_response: response, subject: req.body.subject, body: req.body.html });
+        }
+        mailer.close();
+    });
 })
 
 module.exports = router
