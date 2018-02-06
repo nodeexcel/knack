6f8fcf7cd96eb819ae04acc5cab6143a6f1d578f
ashutosh_m <reactexcel>
2018-02-06 15:50:07 +0530

    attachment optiponal in send mail api

--- routes/send_mail.js
+++ routes/send_mail.js
@@ -2,12 +2,20 @@ var nodemailer = require("nodemailer");
 var smtpTransport = require("nodemailer-smtp-transport");
 var express = require('express')
 var router = express.Router()
-var multer  = require('multer')
+var multer = require('multer')
 var upload = multer()
 var multiparty = require('multiparty');
 var fs = require('fs');
 
-router.post('/',function (req, res) {
+router.post('/', function(req, res) {
+    let attachments = []
+    if (req.body.filename) {
+        let attachment = {
+            "filename": req.body.filename,
+            "content": new Buffer(req.body.pdf, 'base64')
+        }
+        attachments.push(attachment)
+    }
     let mailer = nodemailer.createTransport(smtpTransport({
         host: req.body.smtp_server,
         port: parseInt(req.body.server_port),
@@ -21,13 +29,10 @@ router.post('/',function (req, res) {
         to: req.body.to,
         subject: req.body.subject,
         html: req.body.html,
-        attachments: [{
-                    filename: req.body.filename,
-                    content: new Buffer(req.body.pdf,'base64')
-                }]
+        attachments: attachments
     }, (error, response) => {
         if (error) {
-            res.json({message:"message not sent successfully",status:0,error:error});
+            res.json({ message: "message not sent successfully", status: 0, error: error });
         } else {
             res.json({ message: "messsage sent successfully", status: 1, email_response: response, subject: req.body.subject, body: req.body.html });
         }
