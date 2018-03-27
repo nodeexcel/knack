f5e490124b8337dde2ae6c7b280c652e506f1cf1
nodeexcel <vaibhav.excel2011@gmail.com>
2018-03-27 12:24:49 +0530

    update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require('cors')
 var bodyParser = require('body-parser');
 var https = require('https');
 var fs = require('fs');
+var http = require('http');
 //var routes = require('./routes/auth.js')(app)
 var privateKey = fs.readFileSync('./s166-62-92-227.secureserver.net.key', 'utf8');
 var certificate = fs.readFileSync('./s166-62-92-227.secureserver.net.crt', 'utf8');
@@ -63,6 +64,10 @@ app.use('/api_call', require('./routes/api_call.js'))
 
 // Start server on HTTP (will use ngrok for HTTPS forwarding)
 let server = https.createServer(credentials, app)
+let httpServer = http.createServer(app);
+httpServer.listen(9000, function() {
+    console.log("Http is listening on port", 9000)
+})
 server.listen(process.env.PORT || 3000, function() {
     console.log('Example app listening on port', process.env.PORT || 3000)
 })
