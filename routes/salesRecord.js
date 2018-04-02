547642fb0b35cec5d9d9a43831b14d21eaf965e1
nodeexcel <vaibhav.excel2011@gmail.com>
2018-04-02 17:50:39 +0530

    update

--- routes/salesRecord.js
+++ routes/salesRecord.js
@@ -73,7 +73,7 @@ router.post('/', function(req, res) {
                                     "name": itemref.name
                                 },
                                 "TaxCodeRef": {
-                                    "value": "NON"
+                                    "value": "TAX"
                                 },
                                 "Qty": sku.field_286_raw
                             }
