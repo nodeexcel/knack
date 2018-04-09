ddce44c905f5f968e3806c269d519d6e2ce11b60
nodeexcel <vaibhav.excel2011@gmail.com>
2018-04-09 19:05:45 +0530

    update for tax

--- routes/inventory.js
+++ routes/inventory.js
@@ -4,79 +4,82 @@ var config = require('../config.json')
 var request = require('request')
 var express = require('express')
 var router = express.Router()
- 
 
 
-router.post('/', function (req, res) {
-  let token ;
 
-  tools.getRelmId().then( (realmId) => {
-        tools.getToken().then((fetchedToken)=>{
-           token=fetchedToken;
-        if(!token) return res.json({error: 'Not authorized'})
-        if(!realmId) return res.json({
-          error: 'No realm ID.  QBO calls only work if the accounting scope was passed!'
-        })
-        var data =  {
-          "Name": req.body.Name,
-          "Sku": req.body.Sku,
-          "Description" :req.body.Description,
-          "IncomeAccountRef": {
-            "name": req.body.IncomeAccountRefName  
-          },
-          "ExpenseAccountRef": {
-            "name": req.body.ExpenseAccountRefName   
-          },
-          "AssetAccountRef": {
-            "name": req.body.AssetAccountRefName
-          },
-          "Type":  req.body.Type,
-          "TrackQtyOnHand": req.body.TrackQtyOnHand,
-          "QtyOnHand":req.body.QtyOnHand,
-          "InvStartDate":req.body.InvStartDate,
-          "PurchaseCost":req.body.PurchaseCost,
-          "UnitPrice": req.body.UnitPrice,
-          "PurchaseDesc": req.body.PurchaseDesc
+router.post('/', function(req, res) {
+    let token;
 
-          };
-          getId.getIncomeAccountRef(req,res).then((incomeAccountRef)=>{
-            data.IncomeAccountRef=incomeAccountRef;
-              getId.getExpenseAccountRef(req,res).then((expenseAccountRef)=>{ 
-               data.ExpenseAccountRef=expenseAccountRef;
-                  getId.getAssetAccountRef(req,res).then((assetAccountRef)=>{ 
-                    data.AssetAccountRef=assetAccountRef
+    tools.getRelmId().then((realmId) => {
+        tools.getToken().then((fetchedToken) => {
+            token = fetchedToken;
+            if (!token) return res.json({ error: 'Not authorized' })
+            if (!realmId) return res.json({
+                error: 'No realm ID.  QBO calls only work if the accounting scope was passed!'
+            })
+            var data = {
+                "Name": req.body.Name,
+                "Sku": req.body.Sku,
+                "Description": req.body.Description,
+                "IncomeAccountRef": {
+                    "name": req.body.IncomeAccountRefName
+                },
+                "ExpenseAccountRef": {
+                    "name": req.body.ExpenseAccountRefName
+                },
+                "AssetAccountRef": {
+                    "name": req.body.AssetAccountRefName
+                },
+                "Type": req.body.Type,
+                "TrackQtyOnHand": req.body.TrackQtyOnHand,
+                "QtyOnHand": req.body.QtyOnHand,
+                "InvStartDate": req.body.InvStartDate,
+                "PurchaseCost": req.body.PurchaseCost,
+                "UnitPrice": req.body.UnitPrice,
+                "PurchaseDesc": req.body.PurchaseDesc,
+                "SalesTaxCodeRef": {
+                    "value": req.body.SalesTaxCodeRefValue || "2",
+                    "name": req.body.SalesTaxCodeRefName || "Exempt"
+                }
+            };
+            getId.getIncomeAccountRef(req, res).then((incomeAccountRef) => {
+                data.IncomeAccountRef = incomeAccountRef;
+                getId.getExpenseAccountRef(req, res).then((expenseAccountRef) => {
+                    data.ExpenseAccountRef = expenseAccountRef;
+                    getId.getAssetAccountRef(req, res).then((assetAccountRef) => {
+                        data.AssetAccountRef = assetAccountRef
                         // Set up API call (with OAuth2 accessToken)
-                        var url = config.api_uri +  realmId + '/item?minorversion=14'
-                        console.log('Making API call to: ' + url )
+                        var url = config.api_uri + realmId + '/item?minorversion=14'
+                        console.log('Making API call to: ' + url)
                         var requestObj = {
-                          url: url,
-                          method:"POST",
-                          json:data,
-                          headers: {
-                            'Authorization': 'Bearer ' + token.accessToken
-                          }
+                            url: url,
+                            method: "POST",
+                            json: data,
+                            headers: {
+                                'Authorization': 'Bearer ' + token.accessToken
+                            }
                         }
 
                         // Make API call
-                        request(requestObj, function (err, response) {
-                          // Check if 401 response was returned - refresh tokens if so!
-                          tools.checkForUnauthorized(req, requestObj, err, response).then(function ({err, response}) {
-                            if(err || response.statusCode != 200) {
-                              return res.json({error: err, statusCode: response.statusCode,response:response.body})
-                            }
-                            // API Call was a success!
-                            tools.saveItemId(req.body.KnackID,response.body.Item.Id)
-                            res.json(response.body)
-                          }, function (err) {
-                            return res.json(err)
-                          })
+                        request(requestObj, function(err, response) {
+                            // Check if 401 response was returned - refresh tokens if so!
+                            tools.checkForUnauthorized(req, requestObj, err, response).then(function({ err, response }) {
+                                if (err || response.statusCode != 200) {
+                                    return res.json({ error: err, statusCode: response.statusCode, response: response.body })
+                                }
+                                // API Call was a success!
+                                tools.saveItemId(req.body.KnackID, response.body.Item.Id)
+                                res.json(response.body)
+                            }, function(err) {
+                                return res.json(err)
+                            })
                         })
-                  }).catch(err=>console.log(err))
-              }).catch(err=>console.log(err))
-          }).catch(err=>console.log(err))
-     })
-  });
-  
+                    }).catch(err => console.log(err))
+                }).catch(err => console.log(err))
+            }).catch(err => console.log(err))
+        })
+    });
+
 })
 
 module.exports = router
