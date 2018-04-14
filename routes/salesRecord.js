810d96c562aea59f57f2722e6a81265298c717c6
nodeexcel <vaibhav.excel2011@gmail.com>
2018-04-14 18:59:35 +0530

    update for test

--- routes/salesRecord.js
+++ routes/salesRecord.js
@@ -27,7 +27,7 @@ router.post('/', function(req, res) {
                             console.log(terms_data, "==================")
                             var url = config.api_uri + realmId + '/invoice?minorversion=14'
                             console.log('Making API call to: ' + url)
-                            console.log(moment(req.body.DueDate).format('YYYY-MM-DD'), "date")
+                            console.log(moment(new Date(req.body.DueDate)).format('YYYY-MM-DD'), "date")
                             data = {
                                 "Line": response_item,
                                 "DueDate": moment(req.body.DueDate).format('YYYY-MM-DD'),
@@ -38,7 +38,7 @@ router.post('/', function(req, res) {
                                 'ShipAddr': req.body.ShipAddr,
                                 'ShipMethodRef': req.body.ShipMethodRef,
                                 'TotalAmt': req.body.TotalAmt,
-                                'ShipDate': moment(req.body.ShipDate).format('YYYY-MM-DD'),
+                                'ShipDate': moment(new Date(req.body.ShipDate)).format('YYYY-MM-DD'),
                                 'TrackingNum': req.body.TrackingNum,
                                 'CustomField': req.body.CustomField,
                                 "SalesTermRef": {
