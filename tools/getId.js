60fa000e60f68bea8d2a0289b9e338f408dceba2
nodeexcel <vaibhav.excel2011@gmail.com>
2018-04-20 13:18:00 +0530

    invoice issue

--- tools/getId.js
+++ tools/getId.js
@@ -235,7 +235,7 @@ var account = function() {
                     // Set up API call (with OAuth2 accessToken)
                     var query = `select * from customer  where DisplayName = '` + name + `'`;
                     console.log(query, "queryyyyyyyyyyyyyy")
-                    var url = config.api_uri + realmId + '/query?query=' + encodeURIComponent(query);
+                    var url = config.api_uri + realmId + '/query?query=' + encodeURI(query);
                     console.log('Making API call to: ' + url)
                     var requestObj = {
                         url: url,
