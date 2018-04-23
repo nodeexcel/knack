b30543020ed006752d10a0141fe6b629cc253e0d
nodeexcel <vaibhav.excel2011@gmail.com>
2018-04-23 16:29:52 +0530

    update invoice functionality

--- routes/salesRecord.js
+++ routes/salesRecord.js
@@ -16,6 +16,7 @@ router.post('/', function(req, res) {
             })
             var data;
 
+            var line = []
 
 
             var invoicNoQuery = `select * from invoice  where DocNumber = '` + req.body.DocNumber + `'`;
@@ -30,7 +31,6 @@ router.post('/', function(req, res) {
                     'Accept': 'application/json',
                 }
             }
-            var line = []
 
             // Make API call
             request(invoicNoQueryrequestObj, function(err, response) {
@@ -43,69 +43,8 @@ router.post('/', function(req, res) {
                         var pars = (JSON.parse(response.body))
                         if (pars.QueryResponse.Invoice.length != 0) {
                             console.log("==============================================================================================================")
-                            getId.getCustomerId(req, res, req.body.customer).then((customerref) => {
-                                var inventory = req.body.inventory;
-                                findInventory(inventory, function(response_item) {
-                                    getId.ItemId(req, res, req.body.SKU).then((itemref) => {
-                                        // getId.getTermId(req, res, req.body.Terms).then((termref) => {
-                                        // Set up API call (with OAuth2 accessToken)
-                                        getId.getTermId(req, res, req.body.SalesTermRef).then((terms_data) => {
-                                            var url = config.api_uri + realmId + '/invoice?minorversion=14'
-                                            console.log('Making API call to: ' + url)
-                                            let date = req.body.DueDate.split('/')
-                                            req.body.DueDate = date[2] + '/' + date[1] + '/' + date[0];
-                                            date = req.body.ShipDate.split('/')
-                                            req.body.ShipDate = date[2] + '/' + date[1] + '/' + date[0]
-                                            data = {
-                                                "Id": pars.QueryResponse.Invoice[0].Id,
-                                                "Line": response_item,
-                                                "DueDate": moment(req.body.DueDate).format('YYYY-MM-DD'),
-                                                "CustomerRef": {
-                                                    "value": customerref.value
-                                                },
-                                                'DocNumber': req.body.DocNumber,
-                                                'ShipAddr': req.body.ShipAddr,
-                                                'ShipMethodRef': req.body.ShipMethodRef,
-                                                'TotalAmt': req.body.TotalAmt,
-                                                'ShipDate': moment(req.body.ShipDate).format('YYYY-MM-DD'),
-                                                'TrackingNum': req.body.TrackingNum,
-                                                'CustomField': req.body.CustomField,
-                                                "SalesTermRef": {
-                                                    "value": terms_data.value
-                                                },
-                                                "CustomerMemo": { value: req.body.CustomerMemo }
-                                            }
-                                            var requestObj = {
-                                                url: url,
-                                                method: "POST",
-                                                json: data,
-                                                headers: {
-                                                    'Authorization': 'Bearer ' + token.accessToken
-                                                }
-                                            }
-
-                                            // Make API call
-                                            request(requestObj, function(err, response) {
-                                                // Check if 401 response was returned - refresh tokens if so!
-                                                tools.checkForUnauthorized(req, requestObj, err, response).then(function({ err, response }) {
-                                                    if (err || response.statusCode != 200) {
-                                                        return res.json({ error: err, statusCode: response.statusCode, response: response.body })
-                                                    }
-                                                    // API Call was a success!
-                                                    //tools.saveCustomerId(req.body.KnackID,response.body.Customer.Id)
-                                                    res.json(response.body)
-                                                }, function(err) {
-                                                    return res.json(err)
-                                                })
-                                            })
-                                            // }).catch(err => console.log(err))
-                                        }).catch(err => console.log(err))
-                                    })
-                                })
-
-                            }).catch(err => console.log(err))
-                        } else {
-                            getId.getCustomerId(req, res, req.body.customer).then((customerref) => {
+                            var line = []
+                            var line = [] getId.getCustomerId(req, res, req.body.customer).then((customerref) => {
                                 var inventory = req.body.inventory;
                                 findInventory(inventory, function(response_item) {
                                     getId.ItemId(req, res, req.body.SKU).then((itemref) => {
@@ -169,6 +108,7 @@ router.post('/', function(req, res) {
                     }
                 })
             })
+
 
 
 
