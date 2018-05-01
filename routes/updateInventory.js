714c08b7536437b0332d7d846fed1aebc566ea73
nodeexcel <vaibhav.excel2011@gmail.com>
2018-05-01 14:43:52 +0530

    update

--- routes/updateInventory.js
+++ routes/updateInventory.js
@@ -37,6 +37,7 @@ router.post('/', function(req, res) {
 
                 // Make API call
                 request(requestObj, function(err, response) {
+                    console.log(response, "pppppppppppppppppppppppppp")
                     // Check if 401 response was returned - refresh tokens if so!
                     tools.checkForUnauthorized(req, requestObj, err, response).then(function({ err, response }) {
                         if (err || response.statusCode != 200) {
