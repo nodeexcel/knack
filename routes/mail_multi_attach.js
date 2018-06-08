bc0862be0a49ae1fdd165024d64738f3079440b2
nodeexcel <vaibhav.excel2011@gmail.com>
2018-06-08 15:25:14 +0530

    updated

--- routes/mail_multi_attach.js
+++ routes/mail_multi_attach.js
@@ -32,8 +32,8 @@ router.post('/', function(req, res) {
         }, (error, response) => {
             if (req.body.attachments.length != 0) {
                 _.forEach(req.body.attachments, (val, key) => {
+                    console.log(val,"kkkkkkkkkkkkkkkkkkkkkkkkkkkk")
                     fs.unlink(val.name, function(err) {
-                        console.log(val.name,"kkkkkkkkkkkkkkkkkkkkkkkkkkkk")
                         if (err) {
                             console.log(err)
                         }
