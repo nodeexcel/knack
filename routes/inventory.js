03c4d694dfbee18ecc2188585163a0d5a0a4e732
nodeexcel <vaibhav.excel2011@gmail.com>
2018-06-12 16:39:03 +0530

    updated

--- routes/inventory.js
+++ routes/inventory.js
@@ -59,7 +59,7 @@ router.post('/', function(req, res) {
                                 'Authorization': 'Bearer ' + token.accessToken
                             }
                         }
-
+                        console.log(data,"pppppppppppppppppppppppppppppp")
                         // Make API call
                         request(requestObj, function(err, response) {
                             // Check if 401 response was returned - refresh tokens if so!
